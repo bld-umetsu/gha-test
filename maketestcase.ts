263d432853b84f0bd1a1945743686869967420e5
bld-umetsu <bld_umetsu@works-hi.co.jp>
2022-05-10 14:46:37 +0900

    BOT-xx1: Change test case

--- maketestcase.ts
+++ maketestcase.ts
@@ -1,37 +1,42 @@
 const fs = require('fs');
 
-interface pkg {name: string; test: string, min: string, max: string}
+interface pkg {name: string; test: string, min: string, max: string, info: string}
 var pkgdt: pkg[] = [  
     // the case of 'OK'
-    {name: "ok-01", test: "3.3.3", min: "1.0.0", max: "2.2.2"}, // major ^, miner ^, patch ^
-    {name: "ok-02", test: "3.3.3", min: "1.4.4", max: "2.5.5"}, // major ^, miner v, patch v 
-    {name: "ok-03", test: "3.3.3", min: "1.4.5", max: "2.5.4"}, // major ^, miner v, patch v (2)
-    {name: "ok-04", test: "3.3.3", min: "3.0.0", max: "3.2.2"}, // major =, miner ^, patch ^
-    {name: "ok-05", test: "3.3.3", min: "3.0.4", max: "3.2.4"}, // major =, miner ^, patch v
-    {name: "ok-06", test: "3.3.3", min: "3.1.5", max: "3.2.2"}, // major =, miner ^, patch v (2)
-    {name: "ok-07", test: "3.3.3", min: "3.3.1", max: "3.3.2"}, // major =, miner =, patch ^
-    {name: "ok-08", test: "3.3.3", min: "3.3.4", max: "3.3.5"}, // major =, miner =, patch v 
-    {name: "ok-09", test: "3.3.3", min: "1.0.0", max: "3.2.2"}, // major v, miner v, patch v
-    {name: "ok-10", test: "3.3.3", min: "4.0.1", max: "4.1.2"}, // major v, miner ^, patch ^ 
-    {name: "ok-11", test: "3.3.3", min: "5.1.4", max: "6.2.0"}, // major v, miner ^, patch ^ (2)
-    {name: "ok-12", test: "13.3.3", min: "13.4.4", max: "13.5.5"}, // major =, miner v, patch v
-    {name: "ok-13", test: "13.3.3", min: "13.4.2", max: "13.5.1"}, // major =, miner v, patch ^
-    {name: "ok-14", test: "13.3.3", min: "13.4.2", max: "13.2.0"}, // major =, miner v, patch ^ (2)
-    {name: "ok-15", test: "13.23.3", min: "13.23.4", max: "13.23.5"}, // major =, miner =, patch v same as 'ok-08'
-    {name: "ok-16", test: "13.23.3", min: "13.23.0", max: "13.23.2"}, // major =, miner =, patch ^ same as 'ok-07' 
-    {name: "ok-17", test: "3.3.3.3", min: "2.3.0", max: "2.3.4"}, // longer version 
-    {name: "ok-18", test: "3.3.3.3", min: "3.3.0", max: "3.3.3"}, // longer version (2)
-    {name: "ok-19", test: "3.3.3-test", min: "2.3.0", max: "2.3.3"}, // include string
-    {name: "ok-20", test: "3.3.3-test", min: "3.3.0", max: "3.3.2"}, // include string (2)
+    {name: "ok-01", test: "3.3.3", min: "1.2.0", max: "2.4.5", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲内<br>パッチバージョン：範囲内"}, // major ^, miner in, patch in 
+    {name: "ok-02", test: "3.3.3", min: "1.0.0", max: "2.2.2", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より大"}, // major ^, miner ^, patch ^
+    {name: "ok-03", test: "3.3.3", min: "1.0.2", max: "2.2.1", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より大,範囲maxとmin逆転"}, // major ^, miner ^, patch ^ (2)
+    {name: "ok-04", test: "3.3.3", min: "1.4.4", max: "2.5.5", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より小"}, // major ^, miner v, patch v 
+    {name: "ok-05", test: "3.3.3", min: "1.4.5", max: "2.5.4", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より小,範囲maxとmin逆転"}, // major ^, miner v, patch v (2)
+    {name: "ok-06", test: "3.3.3", min: "3.0.0", max: "3.2.2", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より大"}, // major =, miner ^, patch ^
+    {name: "ok-07", test: "3.3.3", min: "3.0.2", max: "3.2.1", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より大,範囲maxとmin逆転"}, // major =, miner ^, patch ^ (2)
+    {name: "ok-08", test: "3.3.3", min: "3.0.4", max: "3.2.4", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より小"}, // major =, miner ^, patch v
+    {name: "ok-09", test: "3.3.3", min: "3.1.5", max: "3.2.2", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より小,範囲maxとmin逆転"}, // major =, miner ^, patch v (2)
+    {name: "ok-10", test: "3.3.3", min: "3.3.1", max: "3.3.2", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲より大"}, // major =, miner =, patch ^
+    {name: "ok-11", test: "3.3.3", min: "3.3.4", max: "3.3.5", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲より小"}, // major =, miner =, patch v 
+    {name: "ok-12", test: "3.3.3", min: "4.2.1", max: "5.4.5", info: "メジャーバージョン：範囲より小<br>マイナーバージョン：範囲内<br>パッチバージョン：範囲内"}, // major v, miner in, patch in
+    {name: "ok-13", test: "3.3.3", min: "4.4.4", max: "5.5.5", info: "メジャーバージョン：範囲より小<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より小"}, // major v, miner v, patch v
+    {name: "ok-14", test: "3.3.3", min: "4.4.6", max: "5.5.4", info: "メジャーバージョン：範囲より小<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より小,範囲maxとmin逆転"}, // major v, miner v, patch v (2)
+    {name: "ok-15", test: "3.3.3", min: "4.0.1", max: "4.1.2", info: "メジャーバージョン：範囲より小<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より大"}, // major v, miner ^, patch ^ 
+    {name: "ok-16", test: "3.3.3", min: "5.1.2", max: "6.2.0", info: "メジャーバージョン：範囲より小<br>マイナーバージョン：範囲より大<br>パッチバージョン：範囲より大,範囲maxとmin逆転"}, // major v, miner ^, patch ^ (2)
+    {name: "ok-17", test: "13.3.3", min: "13.4.4", max: "13.5.5", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より小"}, // major =, miner v, patch v
+    {name: "ok-18", test: "13.3.3", min: "13.4.5", max: "13.5.4", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より小,範囲maxとmin逆転"}, // major =, miner v, patch v (2)
+    {name: "ok-19", test: "13.3.3", min: "13.4.2", max: "13.5.1", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より大"}, // major =, miner v, patch ^
+    {name: "ok-20", test: "13.3.3", min: "13.4.2", max: "13.2.0", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲より小<br>パッチバージョン：範囲より大,範囲maxとmin逆転"}, // major =, miner v, patch ^ (2)
+    {name: "ok-21", test: "3.3.3.3", min: "2.3.0", max: "2.3.4", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲内<br>その他バージョン付加"}, // longer version 
+    {name: "ok-22", test: "3.3.3.3", min: "3.3.0", max: "3.3.3", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲maxと同じ<br>その他バージョン付加"}, // longer version (2)
+    {name: "ok-23", test: "3.3.3-test", min: "2.3.0", max: "2.3.3", info: "メジャーバージョン：範囲より大<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲maxと同じ+文字列付加"}, // include string
+    {name: "ok-24", test: "3.3.3-test", min: "3.3.0", max: "3.3.2", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲より大+文字列付加"}, // include string (2)
     // the case of 'NG'
-    {name: "ng-01", test: "3.3.3", min: "2.2.2", max: "4.4.4"}, // major in, miner in, patch in
-    {name: "ng-02", test: "3.3.3", min: "3.2.2", max: "3.4.4"}, // major =, miner in, patch in
-    {name: "ng-03", test: "3.3.3", min: "3.3.2", max: "3.3.4"}, // major =, miner =, patch in
-    {name: "ng-04", test: "3.3.3", min: "3.3.3", max: "3.3.4"}, // major =, miner =, patch=min
-    {name: "ng-05", test: "3.3.3", min: "3.3.2", max: "3.3.3"}, // major =, miner =, patch=max
-    {name: "ng-06", test: "3.3.3-test", min: "3.3.2", max: "3.3.3"} // major =, miner =, patch=max include string
+    {name: "ng-01", test: "3.3.3", min: "2.2.2", max: "4.4.4", info: "メジャーバージョン：範囲内<br>マイナーバージョン：範囲内<br>パッチバージョン：範囲内"}, // major in, miner in, patch in
+    {name: "ng-02", test: "3.3.3", min: "3.2.2", max: "3.4.4", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲内<br>パッチバージョン：範囲内"}, // major =, miner in, patch in
+    {name: "ng-03", test: "3.3.3", min: "3.3.2", max: "3.3.4", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲内"}, // major =, miner =, patch in
+    {name: "ng-04", test: "3.3.3", min: "3.3.3", max: "3.3.4", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲minと同じ"}, // major =, miner =, patch=min
+    {name: "ng-05", test: "3.3.3", min: "3.3.2", max: "3.3.3", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲maxと同じ"}, // major =, miner =, patch=max
+    {name: "ng-06", test: "3.3.3-test", min: "3.3.2", max: "3.3.3", info: "メジャーバージョン：範囲と同じ<br>マイナーバージョン：範囲と同じ<br>パッチバージョン：範囲maxと同じ+文字列付加"} // major =, miner =, patch=max include string
 ]
 
+// create test of vulnerabilityPackage.json
 var data:string = "[\n"
 for(var i = 0; i < pkgdt.length; i++) {
     data = data + "\t{ \"pkgnm\": \"" + pkgdt[i].name + "\", ";
@@ -49,6 +54,7 @@ fs.writeFile("vulnerabilityPackage.json", data, function(err){
     }
 })
 
+// create test of package-lock.json
 var txt:string = "{\n\t\"name\": \"test-package-lock\",\n\t\"version\": \"0.0.1\",\n\t\"lockfileVersion\": 2,\n\t\"requires\": true,\n\t\"packages\": {\n\t\t\"\": {\n\t\t\t\"devDependencies\": {\n";
 for(var i = 0; i < pkgdt.length; i++) {
     txt = txt + "\t\t\t\t\"" + pkgdt[i].name + "\": \"^" + pkgdt[i].min + "\"";
@@ -61,7 +67,7 @@ txt = txt + "\t\t\t}\n\t\t},\n";
 for(var i = 0; i < pkgdt.length; i++) {
     txt = txt + "\t\t\"node_modules/" + pkgdt[i].name + "\": {\n";
     txt = txt + "\t\t\t\"version\": \"" + pkgdt[i].test + "\",\n";
-    txt = txt + "\t\t\t\"resolved\": \"https://hogehoge.org/" + pkgdt[i].name + pkgdt[i].test + ".tgz\",\n";
+    txt = txt + "\t\t\t\"resolved\": \"https://hogehoge.org/" + pkgdt[i].name + "_" + pkgdt[i].test + ".tgz\",\n";
     txt = txt + "\t\t\t\"dev\": true\n\t\t}";
     if(i < pkgdt.length-1) {
         txt = txt + ",";
@@ -71,7 +77,7 @@ txt = txt + "\t},\n\t\"dependencies\": {\n";
 for(var i = 0; i < pkgdt.length; i++) {
     txt = txt + "\t\t\"" + pkgdt[i].name + "\": {\n";
     txt = txt + "\t\t\t\"version\": \"" + pkgdt[i].test + "\",\n";
-    txt = txt + "\t\t\t\"resolved\": \"https://hogehoge.org/" + pkgdt[i].name + pkgdt[i].test + ".tgz\",\n";
+    txt = txt + "\t\t\t\"resolved\": \"https://hogehoge.org/" + pkgdt[i].name + "_" + pkgdt[i].test + ".tgz\",\n";
     txt = txt + "\t\t\t\"dev\": true\n\t\t}";
     if(i < pkgdt.length-1) {
         txt = txt + ",";
@@ -84,4 +90,35 @@ fs.writeFile("testpackage-lock.json", txt, function(err){
         return console.log("error");
     }
 })
+
+// create test case document
+var td:string = "## バージョン範囲を組み合わせテスト\n\n";
+td = td + "下記表の\'手順\'記載の `package name` と `test version` を package-lock.json に記載し、<br>";
+td = td + " `min of range` と `max of range` のバージョン範囲を本github action の設定用jsonに記載し、<br>";
+td = td + "pull request を行い、本github action を動作させる。\n";
+td = td + "| No | 手順 | 期待値 | 特記 |\n";
+td = td + "|----|--------------------|--------------------|------|\n";
+for(var i = 0; i < pkgdt.length; i++) {
+    td = td + "| " + (i+1) + " | ";
+    td = td + pkgdt[i].info + "<br>";
+    td = td + "- package name: " + pkgdt[i].name + "<br>";
+    td = td + "- test version: " + pkgdt[i].test + "<br>";
+    td = td + "- min of range: " + pkgdt[i].min + "<br>";
+    td = td + "- max of range: " + pkgdt[i].max + "<br>";
+    if(pkgdt[i].name.indexOf('ok') === 0) {
+        td = td + " | チェックがOKになること。 | |\n";
+    }
+    else if(pkgdt[i].name.indexOf('ng') === 0) {
+        td = td + " | チェックがNGになること。 | |\n";
+    }
+    else {
+        td = td + " | | |\n";
+    }
+}
+fs.writeFile("vuln_version_testcase.md", td, function(err){
+    if(err){
+        return console.log("error");
+    }
+})
+
 
